b74fed6e3abdf5c980b43fc3dcbe85c5fc89baa3
Ragul84 <86684269+Ragul84@users.noreply.github.com>
2024-04-04 16:45:42 +0530

    Sdxe

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { FaRocket } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 import { Toaster, toast } from 'react-hot-toast'
 
